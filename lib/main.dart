6755d829e7b31983f027dc6397ce6de3b48993b0
Lamine Ndour <juniorndour1503@gmail.com>
2024-09-24 14:40:13 -0300

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-        home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.pink, Colors.purple]),
-        ),
-        child: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    )),
+    MaterialApp(home: Scaffold(body: GradienContainer())),
   );
+}
+
+class GradienContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.pink, Colors.purple]),
+      ),
+      child: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
 }

a4e4bfc7fed4b77fbb6c0b9e48bb65c9819f7642
laminedev-creator <juniorndour1503@gmail.com>
2024-09-24 12:32:40 -0300

    added const

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World'),
